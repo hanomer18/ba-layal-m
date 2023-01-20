a0ef28f0f05775df4fba6cf6e8832060bf4dab6e
hanomer18 <96796878+hanomer18@users.noreply.github.com>
2023-01-20 17:01:19 +0300

    Add files via upload

--- indexo.js
+++ indexo.js
@@ -1,4 +1,6 @@
 
+
+
 function addItem() {
 
     var ul = document.getElementById("dynamic-list");
@@ -11,10 +13,20 @@ function addItem() {
 
 }
 
+
 function removeItem() {
     var ul = document.getElementById("dynamic-list");
     var inputtext = document.getElementById("inputtext");
     var item = document.getElementById(inputtext.value);
     ul.removeChild(item);
-    inputtext.value = ""
+    inputtext.value = "";
+
 }
+
+document.getElementById("aaa").addEventListener("dblclick", function () {
+    var ul = document.getElementById("dynamic-list");
+    var inputtext = document.getElementById("inputtext");
+    var item = document.getElementById(inputtext.value);
+    ul.remove(item);
+    inputtext.value = "";
+})
